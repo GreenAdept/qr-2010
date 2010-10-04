0a7b2035f03046896e7baff2262bd3b9d12ecb0c
greenadept <greenadept@244d4fc7-20c2-5101-ab05-2a5c32b82991>
2010-10-04 19:34:38 +0000

    Initial upload. Settings file will be broken for individual users.

---
+++ urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^Tutorial/', include('Tutorial.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^polls/', include('qr.polls.urls')),
+    (r'^admin/', include(admin.site.urls)),
+)
