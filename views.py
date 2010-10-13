126d77327ec29c11d17b6948d208431b1685132c
dlr365 <dlr365@gmail.com>
2010-10-13 16:26:22 +0000

    Added test versions of the Google maps API (/pymap/) and a Python QR code generator (/code/<data>).

---
+++ views.py
@@ -0,0 +1,32 @@
+
+from django.http import HttpResponse
+
+import Image
+import thirdparty.PyQRNative as pyqr
+def qr_code(request, data):
+    
+    qr = pyqr.QRCode(2, pyqr.QRErrorCorrectLevel.L)
+    qr.addData(data)
+    qr.make()
+    
+    response = HttpResponse(mimetype='image/png')
+    qr.makeImage().save(response, "PNG")
+    return response
+
+
+import thirdparty.pymaps as pymaps
+def pymaps_map(request):
+    
+    map_obj = pymaps.PyMap()
+    map_obj.maps[0].center = (51.270373,-113.98766)
+    map_obj.maps[0].zoom = "15"
+    
+    icon2 = pymaps.Icon('icon2')               # create an additional icon
+    icon2.image = "http://labs.google.com/ridefinder/images/mm_20_blue.png" # for testing only!
+    icon2.shadow = "http://labs.google.com/ridefinder/images/mm_20_shadow.png" # do not hotlink from your web page!
+    map_obj.addicon(icon2)
+    
+    map_obj.maps[0].setpoint([51.270373,-113.98766, 'Dougs house!', 'icon2'])
+    
+    return HttpResponse(map_obj.showhtml())
+
