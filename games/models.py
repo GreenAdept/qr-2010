b502cb64ec3d529443a20df9acf37cb4099a09c5
greenadept <greenadept@244d4fc7-20c2-5101-ab05-2a5c32b82991>
2010-10-19 01:00:54 +0000

    Location table coded.

--- games/models.py
+++ games/models.py
@@ -10,14 +10,23 @@ GAME_TYPES = (
               )
 GAME_TEMPLATE_DIR = local_settings.LOCAL_ROOT_DIR + 'game_template_dir/'
 # Create your models here.
-class Games(models.Model):
+class Game(models.Model):
     game_type = models.CharField(max_length=2, choices=GAME_TYPES)
     is_public = models.BooleanField()
-    location = models.CharField(max_length=45)
+    city = models.CharField(max_length=45)
     created_by = models.OneToOneField(User)
     created = models.DateTimeField()
     template_directory = models.FilePathField(path=GAME_TEMPLATE_DIR, recursive=True, blank=True,)
     
     class Meta:
-        db_table = 'games'
+        db_table = 'game'
         get_latest_by = "created"
+
+class Location(models.Model):
+    QR_code = models.FileField(upload_to='qr_codes')
+    location = models.CharField(max_length=45)
+    clue = models.TextField(blank=True)
+    created = models.DateTimeField()
+    visible = models.DateTimeField()
+    expires = models.DateTimeField()
+    gameID = models.ForeignKey('Game')
