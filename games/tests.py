496dbb16bcdc6d30b890544eb7a061ed469ba033
dlr365 <dlr365@gmail.com>
2010-11-21 22:39:37 +0000

    - added views: 'game_details' - added unit tests for views: 'game_list', 'create_game', 'game_details' (details not fully done yet) - added models: 'Player' for linking games and users - cleaned up game templates - added unit test fixture data

--- games/tests.py
+++ games/tests.py
@@ -1,23 +1,184 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
 
-Replace these with more appropriate tests for your application.
-"""
-
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.utils import simplejson
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from qr.games.models import *
+from qr import settings
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+def check_context(testcase, context_items):
+    try:
+        for item in context_items:
+            testcase.response.context[item]
+    except KeyError:
+        testcase.fail('%s not in context' % item)
 
->>> 1 + 1 == 2
-True
-"""}
+class TestView_game_list(TestCase):
+    fixtures = ['test_data/users.xml',
+                'test_data/games.xml']
+    url = reverse('game_list')
+    
+    def setUp(self):
+        self.response = self.client.get(self.url)
+        
+        # make sure we have some games & users in the fixture
+        self.assertTrue(Game.objects.all().count() > 0)
+        self.assertTrue(User.objects.all().count() > 0)
+    
+    def test_correct_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'games/list.html')
+    
+    def test_context(self):
+        check_context(self, ['game_list'])
+    
+    def test_no_games_listed(self):
+        # delete the games & then get a new list
+        Game.objects.all().delete()
+        self.response = self.client.get(self.url)
+        self.assertContains(self.response, 'No games')
+    
+    def test_all_games_listed(self):
+        for game in Game.objects.all():
+            self.assertContains(self.response, game.city)
+
+    def test_anon_user_gets_no_edit_links(self):
+        for game in Game.objects.all():
+            edit_link = reverse('game_edit',
+                                kwargs={'game_id':game.id})
+            self.assertNotContains(self.response, edit_link)
+
+    def test_logged_in_user_gets_correct_edit_links(self):
+        # login a user, then get a new list
+        user = User.objects.get(pk=1)
+        self.assertTrue(
+            self.client.login(username=user.username,
+                              password=user.username))
+        self.response = self.client.get(self.url)
+        
+        # for each game, check if the edit link exists when
+        # it should. Also, we should have at least one of each
+        # (otherwise the test data we're using isn't very useful)
+        contain_count = 0
+        not_contain_count = 0
+        for game in Game.objects.all():
+            edit_link = reverse('game_edit',
+                                kwargs={'game_id':game.id})
+            if game.created_by == user:
+                self.assertContains(self.response, edit_link)
+                contain_count += 1
+            else:
+                self.assertNotContains(self.response, edit_link)
+                not_contain_count += 1
+        
+        self.assertTrue(contain_count > 0)
+        self.assertTrue(not_contain_count > 0)
+        
+
+
+class TestView_create_game(TestCase):
+    fixtures = ['test_data/users.xml']
+    url = reverse('game_create')
+    
+    def setUp(self):
+        # most tests will need a response from a logged-in user
+        self.user = User.objects.get(pk=1)
+        self.assertTrue(
+            self.client.login(username=self.user.username,
+                              password=self.user.username))
+        self.response = self.client.get(self.url)
+
+    def test_correct_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'games/create.html')
+
+    def test_context(self):
+        check_context(self, ['form', 'gmap_js'])
+
+    def test_game_creation(self):
+        # shouldn't be any games yet
+        self.assertEqual(Game.objects.count(), 0)
+        
+        # POST data to create a game
+        point_data = [ { 'id' : '0',
+                         'lat' : '12.34533',
+                         'lon' : '-0.32112' } ]
+        form_data = { 'game_type' : GAME_TYPES[0][0],
+                      'is_public' : True,
+                      'city' : 'calgary',
+                      'locations' : simplejson.dumps(point_data) }
+        self.response = self.client.post(self.url, form_data, follow=True)
+        
+        # a game should have been created in the database
+        self.assertEqual(Game.objects.count(), 1)
+        game = Game.objects.all()[0]
+        self.assertEqual(form_data['game_type'], game.game_type)
+        self.assertEqual(form_data['is_public'], game.is_public)
+        self.assertEqual(form_data['city'], game.city)
+        self.assertEqual(point_data[0]['lat'], str(game.center_latitude))
+        self.assertEqual(point_data[0]['lon'], str(game.center_longitude))
+        self.assertEqual(self.user, game.created_by)
+        
+        # we should be redirected to the game editing page
+        expUrl = reverse('game_edit', args=(game.id,))
+        self.assertRedirects(self.response, expUrl)
+    
+    def test_anon_user_redirected(self):
+        self.client.logout()
+        self.response = self.client.get(self.url)
+        expUrl = settings.LOGIN_URL + '?next=' + self.url
+        self.assertRedirects(self.response, expUrl)
+
+class TestView_game_details(TestCase):
+    fixtures = ['test_data/users.xml',
+                'test_data/games.xml']
+    url = 'game_details'    # actual URL set in setUp()
+    
+    def setUp(self):
+        # most tests will need a response from a logged-in user,
+        # who is NOT the creator of the game
+        self.game = Game.objects.get(pk=1)
+        self.user = User.objects.get(pk=2)
+        self.assertNotEqual(self.user, self.game.created_by)
+        self.url = reverse(self.url, args=(self.game.pk,))
+        self.assertTrue(
+            self.client.login(username=self.user.username,
+                              password=self.user.username))
+        self.response = self.client.get(self.url)
+
+    def test_correct_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'games/details.html')
+
+    def test_context(self):
+        check_context(self, ['game', 'players', 'can_join_game'])
+
+    def test_game_creator_cannot_join(self):
+        # logout user2 and login the creator of the game
+        self.client.logout()
+        self.user = self.response.context['game'].created_by
+        self.assertTrue(
+            self.client.login(username=self.user.username,
+                              password=self.user.username))
+        self.response = self.client.get(self.url)
+        
+        self.assertFalse(self.response.context['can_join_game'])
+
+    def test_other_user_can_join_game(self):
+        # we are by default not a member of the game,
+        # so we should be able to join it
+        self.assertTrue(self.response.context['can_join_game'])
+    
+    def test_user_cannot_join_game_twice(self):
+        # join the game
+        player = Player(game=self.game, user=self.user)
+        player.save()
+        
+        # get the page again, but we should not be able to join
+        self.response = self.client.get(self.url)
+        self.assertFalse(self.response.context['can_join_game'])
+
+
+
 
--- games/urls.py
+++ games/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^$', 'qr.games.views.game_list'),
-    (r'^create/$', 'qr.games.views.create'),
-    url(r'^(?P<game_id>\d+)/$', 'qr.games.views.location_pick', name='location_pick'),
+    url(r'^$', 'qr.games.views.game_list', name='game_list'),
+    url(r'^create/$', 'qr.games.views.create_game', name='game_create'),
+    url(r'^(?P<game_id>\d+)/edit/$', 'qr.games.views.edit_game', name='game_edit'),
+    url(r'^(?P<game_id>\d+)/$', 'qr.games.views.game_details', name='game_details'),
 )
