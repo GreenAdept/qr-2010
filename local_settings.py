9be30b73c40dc6d67001e66603506d0c412032dc
drew.link11 <drew.link11@gmail.com>
2010-11-29 00:31:34 +0000

    Added qr.tests for testing top level (index, login, ...) functions

--- local_settings.py
+++ local_settings.py
@@ -3,14 +3,14 @@
 
 DEBUG = True
 
-DATABASE_USER=''             # Not used with sqlite3.
-DATABASE_PASSWORD=''         # Not used with sqlite3.
+DATABASE_USER='root'                   # Not used with sqlite3.
+DATABASE_PASSWORD='whitenoise'         # Not used with sqlite3.
 DATABASE_HOST=''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT=''             # Set to empty string for default. Not used with sqlite3.
 
 # set this to the absolute path to your QR site install
 # MAKE SURE TO INCLUDE THE TRAILING SLASH
-LOCAL_ROOT_DIR=''
+LOCAL_ROOT_DIR='C:/Documents and Settings/dell/BitNami DjangoStack projects/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '22kl_=r)4+28hou5)s#=+aunzz1%02!w%y(heiab^*t^$bizhc'
