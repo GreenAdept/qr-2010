1be3e457069f59c35305fe4d85cba9d72b761f08
greenadept <greenadept@gmail.com>
2011-04-04 15:57:17 +0000

    images

---
+++ static/scripts/images.js
@@ -0,0 +1,56 @@
+var data = [
+	{
+        image: '/static/media/index1.jpg',
+        title: 'my second image',
+        description: 'Another caption',
+        link: '/static/media/index1.jpg'
+    },
+    {
+        image: '/static/media/img1.jpg',
+        title: 'my first image',
+        description: 'Lorem ipsum caption',
+        link: '/static/media/img1.jpg'
+    },
+    {
+        image: '/static/media/img2.jpg',
+        title: 'my second image',
+        description: 'Another caption',
+        link: '/static/media/img2.jpg'
+    },
+    {
+        image: '/static/media/img3.jpg',
+        title: 'my second image',
+        description: 'Another caption',
+        link: '/static/media/img3.jpg'
+    },
+    {
+        image: '/static/media/img4.jpg',
+        title: 'my second image',
+        description: 'Another caption',
+        link: '/static/media/img4.jpg'
+    },
+    {
+        image: '/static/media/img5.jpg',
+        title: 'my second image',
+        description: 'Another caption',
+        link: '/static/media/img5.jpg'
+    },
+    {
+        image: '/static/media/img6.jpg',
+        title: 'my second image',
+        description: 'Another caption',
+        link: '/static/media/img6.jpg'
+    },
+    {
+        image: '/static/media/img7.jpg',
+        title: 'my second image',
+        description: 'Another caption',
+        link: '/static/media/img7.jpg'
+    },
+    {
+        image: '/static/media/img8.jpg',
+        title: 'my second image',
+        description: 'Another caption',
+        link: '/static/media/img8.jpg'
+    }
+];
