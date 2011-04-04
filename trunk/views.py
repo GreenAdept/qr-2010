de0dfecff67b8d46e4293f052a2668f62c79b80b
greenadept@gmail.com <greenadept@gmail.com@244d4fc7-20c2-5101-ab05-2a5c32b82991>
2011-04-04 05:58:23 +0000

    Activity stream in the left hand nav bar. Info functions added to all pages.

--- trunk/views.py
+++ trunk/views.py
@@ -3,25 +3,34 @@ from django.contrib.auth import authenticate, login, logout
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
-from qr.games.models import *
+from qr.games.models import Game as Game
+from qr.games.models import ActivityStreamItem as ActivityStream
+from django import template
+from django.contrib.auth.models import User
+import datetime
 
 
 def index(request):
-    num_games = Game.objects.count()
     context = RequestContext(request)
-    context['num_games'] = num_games
+    context = page_info(context)
+    
     return render_to_response('home/index.html', context)
     
 def er(request):
-    return render_to_response('404.html', RequestContext(request))
+	context = RequestContext(request)
+	context = page_info(context)
+	return render_to_response('404.html', context)
 
-def contact(request):    
-    return render_to_response('home/contact.html', RequestContext(request))
+def contact(request):
+	context = RequestContext(request)   
+	context = page_info(context) 
+	return render_to_response('home/contact.html', context)
 
 def site_login(request):
     if request.method == 'GET':
         context = RequestContext(request)
         context['next'] = request.GET['next']
+        context = page_info(context)
         return render_to_response('home/login.html', context)
     
     if request.method == 'POST':
@@ -58,5 +67,25 @@ def site_logout(request):
 
 @login_required
 def user_profile(request, username):
-    context = RequestContext(request)
+    context = RequestContext(request)   
+    context = page_info(context) 
     return render_to_response('users/profile.html', context)
+    
+    
+def page_info(context):
+	num_games = Game.objects.count()
+	context['num_games'] = num_games
+	activity_stream = ActivityStream.objects.all()
+	context['activity_stream'] = activity_stream
+	context['num_users'] = online_users()
+	return context
+	
+def online_users():
+    """
+    Show user that has been login an hour ago.
+    """
+    one_hour_ago = datetime.datetime.now() - datetime.timedelta(hours=1)
+    sql_datetime = datetime.datetime.strftime(one_hour_ago, '%Y-%m-%d %H:%M:%S')
+    users = User.objects.filter(last_login__gt=sql_datetime,
+                                is_active__exact=1).order_by('-last_login')
+    return users.count()
